519e733dd23b6ae8ccad987e97bc02ea08a9849f
U Sen Wan <senwan96@gmail.com>
2024-06-10 12:05:36 +0600

    background parralax and fix button

--- src/pages/home/featured/Featured.jsx
+++ src/pages/home/featured/Featured.jsx
@@ -4,12 +4,12 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20 mb-12">
+        <div className="featured-item bg-fixed text-white pt-8 my-20 mb-12">
             <SectionTitle
             subHeading='Check It Out'
             heading='Featured Item'
             ></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-70 pb-20 pt-12 px-36">
             <div>
                 <img src={featuredImg} alt="" />
             </div>
@@ -17,7 +17,7 @@ const Featured = () => {
                 <p>June 10, 2024</p>
                 <p>Where Can I get Some?</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae repudiandae odit eveniet deleniti enim assumenda, quos doloremque, voluptate et illo ullam quo earum animi blanditiis in veritatis distinctio natus mollitia voluptatum dignissimos porro, vero quasi sed? Cupiditate facere neque harum hic, aliquam, ut, fuga sequi quia veritatis debitis impedit unde.</p>
-                <button className="btn btn-outline">Order Now</button>
+                <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
             </div>
             </div>
         </div>
