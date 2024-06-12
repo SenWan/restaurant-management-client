0009e759f964f312e52ddd4d14880dd2e5003730
U Sen Wan <senwan96@gmail.com>
2024-06-12 10:38:04 +0600

    display different menu category with cove

--- src/pages/home/popularMenu/PopularMenu.jsx
+++ src/pages/home/popularMenu/PopularMenu.jsx
@@ -3,25 +3,22 @@ import MenuItem from "../../shared/menuItem/MenuItem";
 import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu] = useMenu();
-    const popular = menu.filter(item => item.category === 'popular');
-    
-    return (
-        <section className="mb-12">
-            <SectionTitle
-            heading="From Our Menu"
-            subHeading="Popular Item"
-            ></SectionTitle>
-            <div className="grid md:grid-cols-2 gap-10">
-            {
-                    popular.map(item => <MenuItem
-                        key={item._id}
-                        item={item}
-                    ></MenuItem>)
-                }
-            </div>
-        </section>
-    );
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Item"
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,15 +1,37 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../shared/Cover/Cover";
 import menuImg from '../../assets/menu/banner3.jpg';
+import dessertImg from '../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../assets/menu/pizza-bg.jpg';
+import soupImg from '../../assets/menu/soup-bg.jpg';
+import saladImg from '../../assets/menu/salad-bg.jpg';
+import useMenu from "../../hooks/useMenu";
+import SectionTitle from "../../components/sectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory/MenuCategory";
 
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Helmet>
                 <title>Restaurant | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="our menu"></Cover>
+           {/*  main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items */}
+            <MenuCategory  items={desserts} title="Desserts" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"Pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory  items={soup} title={"Soups"} img={soupImg}></MenuCategory>
+            <MenuCategory  items={salad} title={"Salads"} img={saladImg}></MenuCategory>
         </div>
     );
 };
